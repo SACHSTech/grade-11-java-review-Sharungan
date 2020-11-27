1af99995b330a27860f28968fed5cfe44f3a5183
Shar <Sharungan.Umaipalan20@ycdsbk12.ca>
2020-11-27 14:55:48 +0000

    Comments

--- src/gr11review/part1/Review4.java
+++ src/gr11review/part1/Review4.java
@@ -3,6 +3,12 @@ package gr11review.part1;
 import java.io.*;
 import java.text.DecimalFormat;
 
+/** 
+* A program that asks the user for how many items that they want to buy, then asks for the price of each item and calculates and outputs the tax, subtotal and total amount of their purchase.
+*@author: S. Umaipalan
+*
+*/
+
 public class Review4 {
   public static DecimalFormat decimalFormat = new DecimalFormat("0.00");
 
@@ -18,18 +24,27 @@ public class Review4 {
     int intCount;
 
     System.out.println("How many items do you want to buy?");
+    dblSubtotal = 0;
     // Ask how many items the person wants to buy
     intItems = Integer.parseInt(keyboard.readLine());
-    for (intCount = 0; intCount <= intItems; intCount++) {
+    //reads how many items there are
+    for (intCount = 1; intCount <= intItems; intCount++) {
       System.out.println("What is the price for item " + intCount);
-      dblSubtotal = 0;
+      //creates a loop to ask for the prices of each item
       dblPrice = Integer.parseInt(keyboard.readLine());
+      //gets the price of each item
       dblSubtotal = dblSubtotal + dblPrice;
-      dblTax = dblSubtotal * 0.13;
-      dblTotal = dblSubtotal * 1.13;
-      System.out.println("Subtotal " + dblSubtotal);
-      System.out.println("Tax " + dblTax);
-      System.out.println("intTotal " + dblTotal);
+      //Adds the prices of each item together 
     }
+    dblTax = dblSubtotal * 0.13;
+    //how much the tax will be
+    dblTotal = dblSubtotal * 1.13;
+    //how much the total cost with tax is
+    System.out.println("Subtotal " + decimalFormat.format(dblSubtotal));
+    //prints out the subtotal
+    System.out.println("Tax " + decimalFormat.format(dblTax));
+    //prints out how much tax you pay
+    System.out.println("intTotal " + decimalFormat.format(dblTotal));
+    //prints out total amount 
   }
 }
