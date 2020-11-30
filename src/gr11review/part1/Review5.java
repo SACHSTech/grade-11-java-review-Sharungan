0448ce1efc898ef4fe4d881834cbb4c432d3af98
Shar <Sharungan.Umaipalan20@ycdsbk12.ca>
2020-11-30 13:58:53 +0000

    comments

--- src/gr11review/part1/Review5.java
+++ src/gr11review/part1/Review5.java
@@ -1,7 +1,6 @@
 package gr11review.part1;
 
 import java.io.*;
-import java.text.DecimalFormat;
 
 /** 
 * A program that asks for a yearly amount, annual compound interest rate and outputs the number of years.
@@ -10,7 +9,7 @@ import java.text.DecimalFormat;
 */
 
 public class Review5 {
-public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
     BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
     //Variables
@@ -22,21 +21,29 @@ public static void main(String[] args) throws IOException {
     dblYear = 0;
     dblFutureValue = 0;
    
-    System.output.print("What is the target amount");
-    dblTarget = Double.parseDouble(key.readLine());
+    System.out.print("What is the target amount: ");
+    dblTarget = Double.parseDouble(keyboard.readLine());
+    //Asks user for target amount and then reads dblTarget
 
-    System.output.print("Enter the yearly invested amount:");
-    dblYearlyAmount = Double.parseDouble(key.readLine());
-    System.output.print("Enter the compound interest:");
-    dblInterest = Double.parseDouble(key.readLine());
-    System.output.print("Enter the target amount:");
-    dblTarget = Double.parseDouble(key.readLine());
-    
-    while(dblFutureValue < dblTarget ){
-      intYear ++; 
-      dblFutureValue = dblYearlyAmount + (dblYearlyAmount * (dlbInterest/100));
-      System.output.print("It takes this" + dblYear +"s to reach your target amount");
-
+    System.out.print("Enter the yearly invested amount: ");
+    dblYearlyAmount = Double.parseDouble(keyboard.readLine());
+    //Asks user for yearly amount and then reads dblYearlyAmount
+    System.out.print("Enter the compound interest: ");
+    dblInterest = Double.parseDouble(keyboard.readLine());
+    //Asks for interest and then reads dblInterest
+   
+    while(dblFutureValue < dblTarget ){ 
+      //while my Future value is less then the target value continue to run loop
+      dblFutureValue += dblYearlyAmount;
+      //dblFutureValue equals to itself plus dblYearlyAmount
+      dblFutureValue = dblFutureValue + (dblFutureValue*(dblInterest/100));
+      //dblFutureValue is now equal to its self plus its interest rate
+      dblYear += 1;
+      //adds a year for everytime this loop is run
     }
+      System.out.print("It takes this " + dblYear +" years to reach your target amount");
+      //prints out how many years it takes to reach target amount 
+
+    
   }
 }
