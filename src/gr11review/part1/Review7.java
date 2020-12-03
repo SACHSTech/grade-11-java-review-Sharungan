b96877be5b335c73280ddae00a7c00532c03353c
Shar <Sharungan.Umaipalan20@ycdsbk12.ca>
2020-12-03 16:29:58 +0000

    review7 in progress

--- src/gr11review/part1/Review7.java
+++ src/gr11review/part1/Review7.java
@@ -12,6 +12,30 @@ public class Review7 {
   public static void main(String[] args) throws IOException {
     BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
+    String strtheSentence;
+    String StrChar;
+    int intLength;
+    int intCount = 0;
+    int intSpace = 0;
+    char charLet;
+
+
+    System.out.println("Enter a sentence");
+    strtheSentence = keyboard.readLine();
+    intLength = strtheSentence.length();
+    System.out.println("There are " + intLength +"   characters in the sentence");
+    for(intCount = 0; intCount <=intLength; intCount++){ 
+      charLet = strtheSentence.charAt(intCount);
+      if(charLet == ' ' ){ 
+        intSpace ++;
+      }
+      System.out.println("There are " + intSpace + " spaces in the sentence");
+    }else if(charLet == 'a'||charLet =='b'||charLet =='c')
+
+
+       
+
   
+
   }
 }
