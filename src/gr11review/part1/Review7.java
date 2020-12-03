07e2fec3836edee0af7d9681dfead18e4a23536f
Shar <Sharungan.Umaipalan20@ycdsbk12.ca>
2020-12-03 15:35:36 +0000

    Finished review8

--- src/gr11review/part1/Review7.java
+++ src/gr11review/part1/Review7.java
@@ -1,1 +1,17 @@
 package gr11review.part1;
+
+import java.io.*;
+
+/** 
+* A program that asks the user for a sentence and then stores it and collects the following data from it; # of characters, # of spaces, #of letter "a" in the sentence, and the number of odd numbered characters. 
+*@author: S. Umaipalan
+*
+*/
+
+public class Review7 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+  
+  }
+}
--- src/gr11review/part1/Review8.java
+++ src/gr11review/part1/Review8.java
@@ -1,1 +1,50 @@
 package gr11review.part1;
+
+import java.io.*;
+
+/** 
+* A program that does 1000 slot pulls and shows the 3 numbers you pull each time and counts how many triples you get and displays it at the end.  
+*@author: S. Umaipalan
+*
+*/
+
+public class Review8 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+    //variables
+    int intPull1;
+    int intPull2;
+    int intPull3;
+    int intCount;
+    int intTriple;
+
+    intCount = 0;
+    intTriple = 0;
+
+    //loop to pull 1000 times 
+    for(intCount = 0; intCount <= 1000; intCount++){
+      intPull1 = (int)(Math.random()*9) + 0;
+      //gets a random number for pull1
+      intPull2 = (int)(Math.random()*9) + 0;
+      //gets a random number for pull2
+      intPull3 = (int)(Math.random()*9) + 0;
+      //gets a random number for pull3
+      System.out.print(intPull1 + " ");
+      //prints out pull1 results
+      System.out.print(intPull2 + " ");
+      //prints out pull2 results
+      System.out.print(intPull3 + " ");
+      //prints out pull3 results
+      System.out.println("");
+      if(intPull1 == intPull2 && intPull3 == intPull1){
+        intTriple ++; 
+        //Checks if intPull1, intPull2, intPull3 are all equal, if so then add a counter to intTriple
+      }
+    }
+      System.out.println("...");
+      System.out.println("You got "+ intTriple + " Triple Pulls");
+      //Prints out how many triples you got 
+  }
+}
